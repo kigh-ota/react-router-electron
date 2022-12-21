41e377f581dbe0ecfeeaa3ff80d69035a3970830
Kaiichiro <kigh.ota@gmail.com>
2022-12-22 02:43:08 +0900

    install react-router-dom and add some routes

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,11 +1,80 @@
 import { AppBar, Toolbar } from "@mui/material";
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createHashRouter,
+  Link,
+  Navigate,
+  RouterProvider,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+
+function Links() {
+  const navigate = useNavigate();
+  return (
+    <ul>
+      <li>
+        <Link to="/page-a">Page A</Link>
+      </li>
+      <li>
+        <Link to="/page-b">Page B</Link>
+      </li>
+      <li>
+        <Link to="/page-c">Page C</Link>
+      </li>
+      <li>
+        <button onClick={() => navigate(-1)}>Back</button>
+      </li>
+      <li>
+        <button onClick={() => navigate(1)}>Forward</button>
+      </li>
+    </ul>
+  );
+}
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Navigate to="/page-a" />,
+  },
+  {
+    path: "/page-a",
+    element: (
+      <>
+        <AppBar position="sticky">
+          <Toolbar>Page A</Toolbar>
+        </AppBar>
+        <Links />
+      </>
+    ),
+  },
+  {
+    path: "/page-b",
+    element: (
+      <>
+        <AppBar position="sticky">
+          <Toolbar>Page B</Toolbar>
+        </AppBar>
+        <Links />
+      </>
+    ),
+  },
+  {
+    path: "/page-c",
+    element: (
+      <>
+        <AppBar position="sticky">
+          <Toolbar>Page C</Toolbar>
+        </AppBar>
+        <Links />
+      </>
+    ),
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppBar position="sticky">
-      <Toolbar>react-router-electron</Toolbar>
-    </AppBar>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
